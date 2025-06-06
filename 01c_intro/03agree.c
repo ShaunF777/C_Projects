d6e50383c68c4ccbe6d8835088cb18be73706363
ShaunF777 <shaunfr777@gmail.com>
2025-06-06 15:53:12 +0200

    Remove unused files and improve comments in 03agree.c and 05meowforloop.c

--- 01c_intro/03agree.c
+++ 01c_intro/03agree.c
@@ -5,9 +5,9 @@ int main(void)
 {
     char c = get_char("Do you agree? ");
 
-    if  (c == 'y' || c == 'Y')  //Ternary operator can be used to sharthand this
-                                // (condition)          ?   then expressio      :   else expression
-                                //if( c=='y' || c=='Y') ? printf("Agreed.\n")   : if (c == 'n' || c == 'N') ? printf("Not agreed.\n") : ;
+    if  (c == 'y' || c == 'Y')  //Ternary operator can be used to shorthand this
+                                // (condition)          ? then expression       : else expression
+                                //( c=='y' || c=='Y') ? printf("Agreed.\n")   : (c == 'n' || c == 'N') ? printf("Not agreed.\n") : ;
     {
         printf("Agreed.\n");
     }
@@ -15,4 +15,9 @@ int main(void)
     {
         printf("Not agreed.\n");
     }
+
+    char d = get_char("Do you agree? ");
+
+    (d == 'y' || d == 'Y') ? printf("Agreed.\n") : (d == 'n' || d == 'N') ? printf("Not agreed.\n") : printf("Invalid input.\n");
+
 }
--- 01c_intro/05meowforloop.c
+++ 01c_intro/05meowforloop.c
@@ -4,7 +4,9 @@
 int main(void)
 {
     for (int i = 0; i <3; i++)  //Parenthesis now has 3 tasks:
-    //1.init a variable(one time); 2.check boolean expression condition and run loop content; 3. do update at end of loop
+    //1.init a variable(one time); 
+    //2.check boolean expression condition and run loop content; 
+    //3. do update at end of loop
     {
         printf("meow\n");
     } // Curly braces can be left out if only one line of code in for loop
